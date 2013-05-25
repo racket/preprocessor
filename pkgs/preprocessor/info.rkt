bbdd13e90737f467706d0b5824aade1978508c83
Matthew Flatt <mflatt@racket-lang.org>
2013-05-24 21:57:31 -0600

    reorganize into core plus packages
    The "racket" directory contains a pared-back version of the
    repository, roughly.
    
    The "pkgs" directory everything else in the repository, but
    organized into packages.
    
    original commit: b2ebb0a28bf8136e75cd98316c22fe54c30eacb2

--- pkgs/preprocessor/info.rkt
+++ pkgs/preprocessor/info.rkt
@@ -1,5 +1,7 @@
 #lang setup/infotab
 
+(define single-collection "preprocessor")
+
 (define mzscheme-launcher-names     '("mzpp"         "mztext"))
 (define mzscheme-launcher-libraries '("mzpp-run.rkt" "mztext-run.rkt"))
 
